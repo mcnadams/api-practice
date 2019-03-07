05f5c5cb6df1b8d7f61fc07105f6a8c32a419003
mcnadams <mcneilbo@oregonstate.edu>
2019-03-07 15:47:48 -0800

    set up project

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const API_KEY = 'kFxeO3X';
+
+const allEffectsUrl = `https://strainapi.evanbusse.com/${API_KEY}/searchdata/effects`;
+
+let effect = 'aroused';
+const searchByEffectUrl = `https://strainapi.evanbusse.com/${API_KEY}/strains/search/effect/${effect}`;
+
+fetch(allEffectsUrl)
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(body => {
+        console.log(body);
+    })
+    .catch((error) => {
+        console.log('Error!!!', error);
+    });
+
+fetch(searchByEffectUrl)
+    .then(response => {
+        return response.json();
+    })
+    .then(body => {
+        console.log(body);
+    })
+    .catch((error) => {
+        console.log('Error!!!', error);
+    });
