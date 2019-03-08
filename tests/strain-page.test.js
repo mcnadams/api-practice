3b20827f815f28bc74a6a6b9f76ccadbda2ebda8
mcnadams <mcneilbo@oregonstate.edu>
2019-03-08 15:50:11 -0800

    TDD makeDescription

--- tests/strain-page.test.js
+++ tests/strain-page.test.js
@@ -14,6 +14,15 @@ function makeStrainHeader(strain) {
     return template.content;
 }
 
+function makeDescription(strain) {
+    const template = document.createElement('template');
+    template.innerHTML = `
+        <p>${strain.desc}</p>
+    `;
+
+    return template.content;
+}
+
 test('make strain header', assert => {
     const expected = `
         <header id="strain-header">
@@ -22,6 +31,15 @@ test('make strain header', assert => {
         </header>
     `;
     const result = makeStrainHeader(fromNameSearch);
+
+    assert.htmlEqual(result, expected);
+});
+
+test('make description p template', assert => {
+    const expected = `
+        <p>Alaskan Thunder Fuck (also referred to as ATF, Matanuska Thunder Fuck or Matanuska Tundra) is a legendary sativa-dominant strain originating in the Matanuska Valley area of Alaska.  According to the legend, it was originally a Northern California sativa crossed with a Russian ruderalis, but sometime in the late 70s it was crossed with Afghani genetics to make it heartier.  ATF usually presents large, beautifully frosted buds with incredibly strong odors of pine, lemon, menthol, and skunk.  Known for possessing a relaxing yet intensely euphoric high, it is also described as having a “creeper” effect as well as pronounced appetite enhancement.</p>
+    `;
+    const result = makeDescription(fromNameSearch);
 
     assert.htmlEqual(result, expected);
 });
