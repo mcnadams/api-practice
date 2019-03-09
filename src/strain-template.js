da160d566cd3f3921f7d4ced20708742b3ead4f6
mcnadams <mcneilbo@oregonstate.edu>
2019-03-08 17:26:17 -0800

    generate strain effect list dynamically using sample data

--- src/strain-template.js
+++ src/strain-template.js
@@ -1,17 +1,5 @@
 export function makeSingleLi(effect) {
     const template = document.createElement('template');
-    template.innerHTML = `<li>${effect}</li>`;
-    return template.content;
-}
-
-function makeEffectsUl(effects) {
-    const template = document.createElement('template');
-    const ul = document.createElement('ul');
-    effects.forEach(effect => {
-        const dom = makeSingleLi(effect);
-        ul.appendChild(dom);
-    });
-    template.appendChild(ul);
-    console.log('ul', ul);
+    template.innerHTML = `<li id="effect">${effect}</li>`;
     return template.content;
 }
--- src/strain.js
+++ src/strain.js
@@ -1,6 +1,51 @@
+import { makeSingleLi } from './strain-template.js';
+
 const back = document.getElementById('back');
 
 back.addEventListener('click', () => {
     window.history.back();
+});
+
+const allEffects = {
+    'positive': [
+        'Happy',
+        'Euphoric',
+        'Uplifted',
+        'Energetic',
+        'Relaxed'
+    ],
+    'negative': [
+        'Dry Mouth',
+        'Dry Eyes',
+        'Paranoid',
+        'Dizzy'
+    ],
+    'medical': [
+        'Stress',
+        'Depression',
+        'Pain',
+        'Fatigue',
+        'Lack of Appetite',
+        'Headache'
+    ]
+};
+
+const positiveList = document.getElementById('positive-effects-list');
+const negativeList = document.getElementById('negative-effects-list');
+const medicalList = document.getElementById('medical-effects-list');
+
+allEffects.positive.forEach(effect => {
+    const dom = makeSingleLi(effect);
+    positiveList.appendChild(dom);
+});
+
+allEffects.negative.forEach(effect => {
+    const dom = makeSingleLi(effect);
+    negativeList.appendChild(dom);
+});
+
+allEffects.medical.forEach(effect => {
+    const dom = makeSingleLi(effect);
+    medicalList.appendChild(dom);
 });
 
